123f23d646ce5098a4b87cc0aa0183e65a7579a9
dxlswb <103540884+dxlswb@users.noreply.github.com>
2022-04-14 13:33:52 -0400

    Update and rename README.md to GITNARCOS.py

---
+++ GITNARCOS.py
@@ -0,0 +1,7 @@
+print("█▄░█ ▄▀█ █▀█ █▀▀ █▀█ █▀▀ █ ▀█▀")
+print("█░▀█ █▀█ █▀▄ █▄▄ █▄█ █▄█ █ ░█░")
+
+print(" ------------------------------")
+print("auto: anonimogit")
+print("tu ip:192.0.1.231")
+print("no pasará nada nadie podrá ver tu IP")
--- README.md
+++
@@ -1,2 +0,0 @@
-# gitnarco
-Git narco es para q veas cuál es tu IP y nadie verá tu IP 
